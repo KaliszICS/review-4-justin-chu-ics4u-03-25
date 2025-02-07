7a6b1d33e5cbba6170d8ca6f3ba51476d0844a2c
justin-chu-ics4u-03-25 <justin.chu4@student.tdsb.on.ca>
2025-02-07 18:52:53 +0000

    My answers

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,32 +12,101 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Input an integer: ");
+		String testing = scanner.nextLine();
+
+		String added = "5" + testing;
+
+		int convert = Integer.parseInt(added);
+
+		int calculation = convert + 5;
+
+		System.out.println(calculation);
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Input a number: ");
+		String stringy = scanner.nextLine();
+
+		String stringyagain = "4" + stringy + "3";
+
+		double doublestringy = Double.parseDouble(stringyagain);
+
+		double add = doublestringy + 3.4;
+
+		System.out.println(add);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Input a boolean: ");
+		String bowl = scanner.nextLine();
+
+		boolean realbool = Boolean.parseBoolean(bowl);
+
+		boolean swap = !realbool;
+
+		System.out.println(swap);
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Input an integer: ");
+		String inta = scanner.nextLine();
+
+		String threeafter = inta + "3";
+
+		int conversation = Integer.parseInt(threeafter);
+
+		int maths = conversation + 2;
+
+		char letter = (char) maths;
+
+		System.out.println(letter);
+
 		
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Input an integer: ");
+		String sussybaka = scanner.nextLine();
+
+		String combo = sussybaka + "1";
+
+		int bakaboy = Integer.parseInt(combo);
+
+		int mathing = bakaboy / 2;
+
+		double convertatata = (double) mathing;
+
+		System.out.println(convertatata);
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Input a number: ");
+		String brotha = scanner.nextLine();
+
+		String oneplus = "1" + brotha;
+
+		double todouble = Double.parseDouble(oneplus);
+
+		int toint = (int) todouble;
+
+		System.out.println(toint);
 		
 	}
 
